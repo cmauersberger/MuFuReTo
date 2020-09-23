c0154c17091abfac509dbc85948d59325f028df4
Christof Mauersberger <c.mauersberger@googlemail.com>
2020-09-23 08:49:49 +0200

    Add solution with initial layout (wip)

---
+++ src/MuFuReTo/MuFuReTo/Code/ImageFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+public class ImageFile
+{
+    public string Path { get; set; }
+    public string OriginalFilename { get; set; }
+    public DateTime DateTaken { get; set; }
+    public string CameraModel { get; set; }
+    public long FileSize { get; set; }
+    public string NewFilenamePreview { get; set; }
+
+    public string FormattedFileSize 
+    {
+        get
+        {
+            var fullPath = Path + "\\" + OriginalFilename;
+            if (!File.Exists(fullPath))
+            {
+                return "";
+            }
+
+            string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+            double len = new FileInfo(fullPath).Length;
+            var order = 0;
+            while (len >= 1024 && order < sizes.Length - 1)
+            {
+                order++;
+                len /= 1024;
+            }
+
+            return $"{len:0.#} {sizes[order]}";
+        }
+    }
+}
---
+++ src/MuFuReTo/MuFuReTo/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace MuFuReTo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string SelectedFolder = null;
+        public List<ImageFile> ImageFiles = new List<ImageFile>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnOpenFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TxtFolder.Text = SelectedFolder = folderBrowserDialog.SelectedPath;
+                //SelectedFolder = openFileDialog.FileName;
+                ReadAllFiles();
+            }
+        }
+
+        private void ReadAllFiles()
+        {
+            if (string.IsNullOrWhiteSpace(SelectedFolder))
+            {
+                return;
+            }
+
+            if (!Directory.Exists(SelectedFolder))
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(SelectedFolder);
+            var imageFiles = files.Select(file =>
+            {
+                var fileInfo = new FileInfo(file);
+                return new ImageFile
+                {
+                    Path = fileInfo.DirectoryName,
+                    OriginalFilename = fileInfo.Name,
+                    FileSize = fileInfo.Length
+                };
+            }).ToList();
+
+            ImageFiles = imageFiles;
+            dgImageFiles.ItemsSource = ImageFiles; // todo: make auto-binding
+        }
+    }
+}
